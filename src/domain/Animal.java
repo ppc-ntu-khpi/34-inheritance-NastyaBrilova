353c296ea362aab6b13635ba49a17a236086587c
NastyaBrilova <72560282+NastyaBrilova@users.noreply.github.com>
2021-02-03 01:39:22 +0200

    Update Animal.java

--- src/domain/Animal.java
+++ src/domain/Animal.java
@@ -4,25 +4,28 @@ public class Animal {
 
     protected String name;
 
+    protected int age;
     protected int weight;
 
     public Animal() {
-        name="generic animal";
-        weight=25;
-    }
-
-    public void eat() {
-        System.out.println("Animal eating...");
+        name = "generic animal";
+        age = 0;
+        weight = 1;
     }
 
     public void speak() {
         System.out.println("Animal speaking...");
     }
-
-    @Override
-    public String toString() {
-        return "My animal:" + "\nName:\t" + name + "\nWeight:\t" + weight+" kg";
+        
+    public void eat() {
+        System.out.println("Animal eating...");
     }
 
- 
+    public void sleep() {
+        System.out.println("Animal sleeping...");
+    }
+
+    public String toString() {
+        return "Animal:" + "\nName:\t" + name + "\nAge:\t" + age + "\nWeight:\t" + weight;
+    }
 }
