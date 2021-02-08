43625cc99cc33807d6afa5df4dff8f093da94479
NastyaBrilova <72560282+NastyaBrilova@users.noreply.github.com>
2021-02-08 17:26:38 +0200

    Delete search directory

--- search/all_8.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['predator_11',['Predator',['../classdomain_1_1_predator.html',1,'domain']]],
-  ['predator_2ejava_12',['Predator.java',['../_predator_8java.html',1,'']]]
-];
--- search/namespaces_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['test_25',['test',['../namespacetest.html',1,'']]]
-];
